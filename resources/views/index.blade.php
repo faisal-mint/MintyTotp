1b75f52e9cea6ab67be84ec3a151f94b49f96ec9
Shah Faisal <shahsaib2024@gmail.com>
2026-01-13 10:05:33 +0400

    Icon & Favicon Added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>MintyOTP - TOTP Authenticator</title>
+    <link rel="icon" type="image/png" href="{{ asset('MintyTotp-logo.png') }}">
     <style>
         * {
             margin: 0;
